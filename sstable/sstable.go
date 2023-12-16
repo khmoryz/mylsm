5c638c8ed62cbcdf23acfc76491d4e49323ca515
khmoryz <kokkenro0520@gmail.com>
2023-12-16 18:03:06 +0900

    feat(sstable): Support multiple sstable.

--- sstable/sstable.go
+++ sstable/sstable.go
@@ -3,13 +3,17 @@ package sstable
 import (
 	"encoding/binary"
 	"io"
+	"io/fs"
 	"mylsm/memtable"
 	"os"
 	"sort"
+	"strconv"
+	"time"
 )
 
 const dirName = "/tmp/mylsm/"
-const fileName = "sstable.mylsm"
+const suffix = "_sstable.mylsm"
+const maxSstableSize = 1000
 
 type SSTable struct {
 	kvs []kv
@@ -20,8 +24,13 @@ type kv struct {
 	value string
 }
 
+func genFileName() string {
+	now := time.Now().UnixNano()
+	return strconv.FormatInt(now, 10) + suffix
+}
+
 func Flush() error {
-	//TODO:memtable exclusive control
+	//TODO: memtable exclusive control
 
 	// uniq
 	uniq := make(map[string]string, 0)
@@ -42,7 +51,7 @@ func Flush() error {
 		panic(err)
 	}
 
-	f, err := os.OpenFile(dirName+fileName, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0600)
+	f, err := os.OpenFile(dirName+genFileName(), os.O_CREATE|os.O_RDWR|os.O_APPEND, 0600)
 	if err != nil {
 		panic(err)
 	}
@@ -58,44 +67,60 @@ func Flush() error {
 	return nil
 }
 
-func Read(searchKey string) (string, bool) {
-	f, err := os.Open(dirName + fileName)
+func getSstableList() []fs.DirEntry {
+	f, err := os.Open(dirName)
 	if err != nil {
 		panic(err)
 	}
-	defer f.Close()
+	e, err := f.ReadDir(maxSstableSize)
+	if err != nil {
+		panic(err)
+	}
+	return e
+}
 
-	for {
-		// Read key
-		var klen int32
-		if err := binary.Read(f, binary.LittleEndian, &klen); err != nil {
-			if err == io.EOF {
-				break
+// TODO: [portability] Depends on OS behavior that file order is ordered by desc.
+func Read(searchKey string) (string, bool) {
+	for _, s := range getSstableList() {
+
+		f, err := os.Open(dirName + s.Name())
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+
+		for {
+			// Read key
+			var klen int32
+			if err := binary.Read(f, binary.LittleEndian, &klen); err != nil {
+				if err == io.EOF {
+					break
+				}
+				panic(err)
 			}
-			panic(err)
-		}
-		k := make([]byte, klen)
-		if _, err := f.Read(k); err != nil {
-			panic(err)
-		}
-
-		// Read value
-		// TODO: Skipping read value if the key matches.
-		var vlen int32
-		if err := binary.Read(f, binary.LittleEndian, &vlen); err != nil {
-			if err == io.EOF {
-				break
+			k := make([]byte, klen)
+			if _, err := f.Read(k); err != nil {
+				panic(err)
 			}
-			panic(err)
-		}
-		v := make([]byte, vlen)
-		if _, err := f.Read(v); err != nil {
-			panic(err)
-		}
 
-		// compare
-		if string(k) == searchKey {
-			return string(v), true
+			// Read value
+			// TODO: [perf] Skipping read value if the key matches.
+			var vlen int32
+			if err := binary.Read(f, binary.LittleEndian, &vlen); err != nil {
+				if err == io.EOF {
+					break
+				}
+				panic(err)
+			}
+			v := make([]byte, vlen)
+			if _, err := f.Read(v); err != nil {
+				panic(err)
+			}
+
+			// compare
+			if string(k) == searchKey {
+				return string(v), true
+			}
 		}
 	}
 
--- sstable/sstable_test.go
+++ sstable/sstable_test.go
@@ -1,21 +1,20 @@
 package sstable
 
 import (
-	"fmt"
 	"mylsm/memtable"
 	"os"
 	"testing"
 )
 
-func TestFlush(t *testing.T) {
+func TestFlushAndRead(t *testing.T) {
 	// clean up
 	os.RemoveAll(dirName)
 
 	// set dummy data
 	memtable.Memt.Kvs = []memtable.Kv{
 		{Key: "a", Value: "foo"},
-		{Key: "c", Value: "hoge"},
-		{Key: "b", Value: "bar"},
+		{Key: "c", Value: "bar"},
+		{Key: "b", Value: "buzz"},
 		{Key: "a", Value: "foo_updated"},
 	}
 
@@ -24,8 +23,50 @@ func TestFlush(t *testing.T) {
 
 	// check
 	v, exist := Read("a")
-	fmt.Println(v, exist)
 	if v != "foo_updated" && exist != true {
+		t.Errorf("unexpected value: %s, exist: %t", v, exist)
+	}
+}
+
+func TestDoubleFlushAndRead(t *testing.T) {
+	// clean up
+	os.RemoveAll(dirName)
+
+	// set first dummy data
+	memtable.Memt.Kvs = []memtable.Kv{
+		{Key: "a", Value: "foo"},
+		{Key: "c", Value: "bar"},
+		{Key: "b", Value: "buzz"},
+		{Key: "a", Value: "foo_updated"},
+	}
+
+	// execute
+	Flush()
+
+	// set first dummy data
+	memtable.Memt.Kvs = []memtable.Kv{
+		{Key: "x", Value: "foo"},
+		{Key: "y", Value: "bar"},
+		{Key: "z", Value: "buzz"},
+		{Key: "b", Value: "buzz_updated_in_the_second"},
+	}
+
+	// execute
+	Flush()
+
+	// check
+	v, exist := Read("a")
+	if v != "foo_updated" && exist != true {
+		t.Errorf("unexpected value: %s, exist: %t", v, exist)
+	}
+
+	v, exist = Read("x")
+	if v != "foo" && exist != true {
+		t.Errorf("unexpected value: %s, exist: %t", v, exist)
+	}
+
+	v, exist = Read("b")
+	if v != "buzz_updated_in_the_second" && exist != true {
 		t.Errorf("unexpected value: %s, exist: %t", v, exist)
 	}
 }
