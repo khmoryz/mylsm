1ccd975d3a1d18e23d89fe9b18b9de4aa981ba33
khmoryz <kokkenro0520@gmail.com>
2024-01-20 17:05:33 +0900

    test: Add data check.

--- sstable/sstable_test.go
+++ sstable/sstable_test.go
@@ -76,12 +76,28 @@ func TestRandomWrite(t *testing.T) {
 	// clean up
 	os.RemoveAll(DirName)
 
-	// set random dummy data
-	cap := 100
+	// set data
+	cap := 30
+	for i := 0; i < cap; i++ {
+		Memt.Kvs = append(Memt.Kvs, Kv{Key: strconv.Itoa(rand.Int()), Value: strconv.Itoa(rand.Int())})
+	}
+	Memt.Kvs = append(Memt.Kvs, Kv{Key: "a", Value: "foo"})
 	for i := 0; i < cap; i++ {
 		Memt.Kvs = append(Memt.Kvs, Kv{Key: strconv.Itoa(rand.Int()), Value: strconv.Itoa(rand.Int())})
 	}
 
 	// execute
 	Flush(&Memt)
+
+	// check: existing data can be retrieved.
+	v, exist := Search("a")
+	if v != "foo" && exist != true {
+		t.Errorf("unexpected value: %s, exist: %t", v, exist)
+	}
+
+	// check: non-existent data can't be retrieved.
+	v, exist = Search("null!!")
+	if v != "" && exist != false {
+		t.Errorf("unexpected value: %s, exist: %t", v, exist)
+	}
 }
